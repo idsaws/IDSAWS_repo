f1788bbd2380b0681c922ec33593ec1e6e5fd0d5
IDSAWS <Norman.Branitsky@irondata.com>
2014-09-15 21:33:01 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/IDSAWS/metadata.rb
@@ -0,0 +1,7 @@
+name             'IDSAWS'
+maintainer       'Iron Data Solutions'
+maintainer_email 'Norman.Branitsky@irondata.com'
+license          'All rights reserved'
+description      'Installs/Configures IDSAWS'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
